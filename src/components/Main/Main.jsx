21486cf809e7fd60416b978ee6a26e4eb85dc7f9
inesherminio <inesherminio@gmail.com>
2023-10-06 11:21:37 +0100

    Updated about me

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,49 +1,51 @@
-import React, { Component } from "react";
-import "./Main.css";
-import photo from "../../assets/foto.jpg";
-import { BsLinkedin } from "react-icons/bs";
-import { BsGithub } from "react-icons/bs";
-import { MdEmail } from "react-icons/md";
+import React, { Component } from 'react';
+import './Main.css';
+import photo from '../../assets/foto.jpg';
+import { BsLinkedin } from 'react-icons/bs';
+import { BsGithub } from 'react-icons/bs';
+import { MdEmail } from 'react-icons/md';
 
 class Main extends Component {
   render() {
     return (
-      <section className="main-container" id="about-me">
-        <div className="info-container">
+      <section className='main-container' id='about-me'>
+        <div className='info-container'>
           <img
             src={photo}
-            alt="Inês Hermínio"
-            className="my-pic"
-            loading="lazy"
+            alt='Inês Hermínio'
+            className='my-pic'
+            loading='lazy'
           />
-          <h1 className="my-name">Inês Hermínio</h1>
-          <p className="my-intro">
-            I am a{" "}
-            <span className="highlited-info">Full Stack Web Developer</span> 👩‍💻,
-            transitioning from a career in Marketing 👩‍💼. <br />I am also a
-            traveler ✈️, begginer drummer 🥁 and experienced fencer 🤺.
+          <h1 className='my-name'>Inês Hermínio</h1>
+          <p className='my-intro'>
+            I am a{' '}
+            <span className='highlited-info'>Full Stack Web Developer</span> 👩‍💻,
+            with a background in Marketing and Strategy 👩‍💼. <br />I am an
+            analytic, but also creative person, responsible and curious 🛠️. I love
+            to travel ✈️, was on the National Fencing team for 20y 🤺 and am now
+            a newbie on Crossfit 🏋.
           </p>
-          <div className="icon-container">
+          <div className='icon-container'>
             <a
-              href="https://linkedin.com/in/ines-herminio"
-              target="_blank"
-              rel="noopener noreferrer"
+              href='https://linkedin.com/in/ines-herminio'
+              target='_blank'
+              rel='noopener noreferrer'
             >
-              <BsLinkedin size={28} className="contact-icon" />
+              <BsLinkedin size={28} className='contact-icon' />
             </a>
             <a
-              href="https://github.com/inesherminio/"
-              target="_blank"
-              rel="noopener noreferrer"
+              href='https://github.com/inesherminio/'
+              target='_blank'
+              rel='noopener noreferrer'
             >
-              <BsGithub size={28} className="contact-icon" />
+              <BsGithub size={28} className='contact-icon' />
             </a>
             <a
-              href="mailto:inesherminio@gmail.com?subject = We want you!"
-              target="_blank"
-              rel="noopener noreferrer"
+              href='mailto:inesherminio@gmail.com?subject = We want you!'
+              target='_blank'
+              rel='noopener noreferrer'
             >
-              <MdEmail size={28} className="contact-icon" />
+              <MdEmail size={28} className='contact-icon' />
             </a>
           </div>
         </div>
